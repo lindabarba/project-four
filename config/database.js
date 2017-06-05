76f279c1dfbffb89ba3f274e05b937ce7b8d742b
Linda Barba <lindabarba13@gmail.com>
2017-06-05 16:35:08 -0700

    fix package.json, add dummy html for App render test

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
 
 mongoose.Promise = Promise;
+// NOT SURE IF WE NEED THIS LINE YET/EVER?
 // mongoose.Promise = gloabl.Promise;
 
 const db = mongoose.connection;
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = Object.assign(
+  //   {}
+  //   );
+  // }
+// }
+
+render() {
+  return (
+    <div>Hello World</div>
+    );
+  }
+}
+
+export default App;
